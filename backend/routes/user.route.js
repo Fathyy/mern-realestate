c6234345ee507240c5bdcad3f516eb38a4f1a643
Fathi Abdi <fathiabdi254@gmail.com>
2024-05-19 21:42:45 +0300

    complete delete listing functionality

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,13 @@
 // we use express to create routes
 import express from 'express';
 import { test } from '../controllers/user.controller';
+import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/test', test)
 router.post('/update/:id', verifyToken, updateUser)
-router.post('/delete/:id', verifyToken, updateUser)
+router.post('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router;
