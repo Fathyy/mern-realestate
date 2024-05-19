64b08bdd034773d71215eb1de2dc78fac73f7bf0
Fathi Abdi <fathiabdi254@gmail.com>
2024-05-19 09:11:31 +0300

    completed image uploading functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,79 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { app } from '../firebase';
+
 
 const Profile = () => {
+const fileRef = useRef(null);
+const { currentUser, loading, error } = useSelector((state) => state.user);
+const [file, setFile] = useState(undefined);
+const [filePerc, setFilePerc] = useState(0);
+const [formData, setFormData] = useState({});
+const [fileUploadError, setFileUploadError] = useState(false);
+const dispatch = useDispatch();
+
+
+// firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
