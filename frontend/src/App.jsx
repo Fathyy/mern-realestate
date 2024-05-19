0777499300d31ef73440dd3635340b9f3fbc98b6
Fathi Abdi <fathiabdi254@gmail.com>
2024-05-19 08:42:30 +0300

    Update the header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { About, Home, Profile, Signin, Signup } from './pages'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Home, Profile, Signin, Signup } from "./pages";
+import PrivateRoute from "./Components/PrivateRoute";
 const App = () => {
   return (
     <BrowserRouter>
-    {/* header component */}
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/signin' element={<Signin/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-
-    </Routes>
+      {/* header component */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
