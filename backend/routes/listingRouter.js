369c0f350740aeca4edda9bf83e650cd8dbbe7ba
Fathi Abdi <fathiabdi254@gmail.com>
2024-05-20 21:25:35 +0300

    create search api route

--- backend/routes/listingRouter.js
+++ backend/routes/listingRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listingController.js'
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listingController.js'
 const router = express.Router();
 import { verifyToken } from '../Utils/verifyUser.js';
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
-router.get('/get/:id', verifyToken, getListing)
+router.get('/get/:id', getListing)
+router.get('/get/:id', getListings)
 
 export default router;
