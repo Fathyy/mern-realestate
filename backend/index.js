431b22b74328f16247a86cf4c3fc2a6035bec34a
Fathi Abdi <fathiabdi254@gmail.com>
2024-09-03 12:20:35 +0300

    made a lot of corrections on the image upload part

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-   console.log("connected to  db")
+   console.log("connected to db")
 })
 .catch((err)=> {
    console.log(err)
@@ -26,8 +26,8 @@ const __dirname = path.resolve();
 
  app.use(cookieParser())
 
- app.listen(3000, ()=>{
-    console.log('Server is running on port 3000')
+ app.listen(5000, ()=>{
+    console.log('Server is running on port 5000')
  })
 
  app.use('/api/user', userRouter)
